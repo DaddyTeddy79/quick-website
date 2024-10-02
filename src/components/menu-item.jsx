57e75b30cba4e0ce3720e34d69ea69e00cadfbf6
DaddyTeddy79 <tcoudiere@yahoo.fr>
2024-10-02 07:28:54 +0200

    Fixing issue with images moving to the left when hovering on one

--- src/components/menu-item.jsx
+++ src/components/menu-item.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MenuItem ({text, svgImage}) {
     return <div className="menu-item">
-        {svgImage}
+        <div className="menu-item-image">{svgImage}</div>
         <div className="menu-item-text">{text}</div>
     </div>
 }
