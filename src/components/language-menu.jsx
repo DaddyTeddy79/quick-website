a073189d01c4fe5675dd6835769d438b7afbfd15
DaddyTeddy79 <tcoudiere@yahoo.fr>
2024-10-16 07:55:10 +0200

    Adding language menu

---
+++ src/components/language-menu.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function LanguageMenu() {
+    return <ul className="language-menu">
+        <li className="language-menu-item-first">FR</li>
+        <li className="language-menu-item-second">NL</li>
+    </ul>
+}
+
+export default LanguageMenu;
--- src/components/right-navigation.jsx
+++ src/components/right-navigation.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { ReactComponent as MyQuickIcon } from "../images/myquick.svg";
 import OrderButton from "./order-button";
+import LanguageMenu from "./language-menu";
 
 function RightNavigation() {
     return <span className="right-navigation">
         <span><MyQuickIcon className="my-quick-icon" /></span>
         <span className="my-quick-label"> | MyQuick</span>
         <span><OrderButton /></span>
+        <LanguageMenu />
     </span>
 }
 
